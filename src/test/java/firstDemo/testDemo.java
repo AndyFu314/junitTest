28b0d70431d06448831c0fbde57b4d7cd89ad6ce
Andy@UDN <f255431@gmail.com>
2015-06-05 18:01:32 +0800

    junit successed

--- src/test/java/firstDemo/testDemo.java
+++ src/test/java/firstDemo/testDemo.java
@@ -1,5 +1,22 @@
 package firstDemo;
 
-public class testDemo {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class testDemo {
+	
+	@Test
+	public void testAdd(){
+		int a = 10;
+		int b = 20;
+		int sum ;
+		
+		sum = a+ b;
+		
+		Assert.assertEquals(30, sum);
+	}
+	
+	
+	
 }
+
